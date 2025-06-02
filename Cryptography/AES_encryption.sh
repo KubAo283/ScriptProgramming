61356e75ae9af7b57af041de3f57abe26676b1a3
KubAo283 <283850@student.pwr.edu.pl>
2025-06-02 11:59:27 +0200

    Cryptography code files

---
+++ Cryptography/AES_encryption.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script encrypts a file using AES encryption with OpenSSL. It requires two arguments:
+# 1. The input file to be encrypted.
+# 2. The output file where the encrypted data will be saved.
+# Usage: ./AES_encryption.sh <input_file> <output_file>
+
+#Checking if the correct number of arguments is passed
+if [ "$#" -ne 2 ]; then
+    echo "Incorrect number of arguments passsed!"
+    exit -1
+fi
+
+# Prompting for password
+read -sp "Enter password: " PASSWORD
+echo
+
+# Encrypt the file using AES-256-CBC
+openssl enc -aes-256-cbc -salt -pbkdf2 -in "$1" -out "$2" -pass pass:"$PASSWORD"
+
+# Checking if the encryption was successful
+if [ $? -eq 0 ]; then
+    echo "Encryption successful, the output will be stored in $2 file."
+else
+    echo "Encryption failed."
+fi
+
---
+++ Cryptography/Digital_Sign.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#This script creates a digital signature for a file using OpenSSL.
+# It requires three arguments:
+# 1. The file to be signed.
+# 2. The private key file (in PEM format).
+# 3. The output file where the signature will be saved.
+# Usage: ./Digital_Sign.sh <file_to_sign> <private_key.pem> <signature_output>
+
+
+#Checking if the correct number of arguments is passed
+if [ "$#" -ne 3 ]; then
+    echo "Incorrect number of arguments passed!" >&2
+    exit -1
+fi
+
+# Assigning arguments to variables
+FILE_TO_SIGN="$1"
+PRIVATE_KEY="$2"
+SIGNATURE_OUTPUT="$3"
+
+#Creating the digital signature using SHA-256
+openssl dgst -sha256 -sign "$PRIVATE_KEY" -out "$SIGNATURE_OUTPUT" "$FILE_TO_SIGN"
+
+#Checking if the signing was successful
+if [ $? -eq 0 ]; then
+    echo "File signed successfully. Signature saved to $SIGNATURE_OUTPUT"
+else
+    echo "Signing failed." >&2
+    exit -1
+fi
